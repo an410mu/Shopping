6ddf623067f6c3987c0c0656ec440dd199974f7a
ted777 <an410mu@gmail.com>
2022-12-13 22:29:44 -0800

    connect mongo with mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import products from "./data/products.js";
+import mongoose from "mongoose";
 // const express = require("express");
 //In package.json, once add type:module, able to use import like frontend
 //need to add .js for file, and change from module.exports to export default
@@ -22,4 +23,18 @@ app.get("/products/:id", (req, res) => {
 });
 
 const port = process.env.PORT || 8000;
-app.listen(port, console.log(`Server listen to port ${port}`));
+
+const start = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(`MongoConnected @: ${conn.connection.host}`);
+    app.listen(port, () => {
+      console.log(`Server is running at port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
