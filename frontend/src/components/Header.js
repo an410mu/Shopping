970728f5de9e13560c0271a3a2a41ed3fed3d26e
ted777 <an410mu@gmail.com>
2023-01-03 11:35:49 -0800

    logout

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,30 +1,97 @@
 import React from "react";
-import { Container, Navbar, Nav } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
+import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../actions/userActions.js";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const history = useNavigate();
+
+  const logoutHandler = () => {
+    console.log("logout");
+    dispatch(logout());
+    history("/");
+  };
+
   return (
-    <>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-        <Container>
-          <NavLink to="/">
-            <Navbar.Brand>Catharine's Shop</Navbar.Brand>
-          </NavLink>
-          <Nav className="ml-auto">
-            <NavLink to="/promotion">
+    <Navbar expand="lg" bg="dark" variant="dark" collapseOnSelect>
+      <Container>
+        <Navbar.Brand href="/">Teddy's Shop</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="/Promotion">
               <i className="fa-solid fa-rectangle-ad"></i>Promotion
-            </NavLink>
-            <NavLink to="/cart">
+            </Nav.Link>
+            <Nav.Link href="/cart">
               <i className="fa-solid fa-cart-shopping"></i>Cart
-            </NavLink>
-            <NavLink to="/login">
-              <i className="fa-solid fa-circle-user"></i>Sign In
-            </NavLink>
+            </Nav.Link>
+            {userInfo ? (
+              <NavDropdown title={userInfo.user.name} id="basic-nav-dropdown">
+                <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
+                <NavDropdown.Item href="/setting">Setting</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item onClick={logoutHandler}>
+                  Log Out
+                </NavDropdown.Item>
+              </NavDropdown>
+            ) : (
+              <Nav.Link href="/login">
+                <i className="fa-solid fa-circle-user"></i>Sign In
+              </Nav.Link>
+            )}
           </Nav>
-        </Container>
-      </Navbar>
-    </>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
 export default Header;
+
+{
+  /* <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+<Container>
+  <NavLink to="/">
+    <Navbar.Brand>Catharine's Shop</Navbar.Brand>
+  </NavLink>
+  <Nav className="ml-auto">
+    <NavLink to="/promotion">
+      <i className="fa-solid fa-rectangle-ad"></i>Promotion
+    </NavLink>
+    <NavLink to="/cart">
+      <i className="fa-solid fa-cart-shopping"></i>Cart
+    </NavLink>
+    {userInfo ? (
+      <Dropdown>
+        <Dropdown.Toggle
+          variant="secondary"
+          id="dropdown-basic"
+          size="sm"
+        >
+          {userInfo.name}
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">
+            Another action
+          </Dropdown.Item>
+          <Dropdown.Item href="#/action-3">
+            Something else
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    ) : (
+      <NavLink to="/login">
+        <i className="fa-solid fa-circle-user"></i>Sign In
+      </NavLink>
+    )}
+  </Nav>
+</Container>
+</Navbar> */
+}
