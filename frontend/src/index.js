5746d6b64ac01f271930fd0348d1b3a0d30f5d96
ted777 <an410mu@gmail.com>
2023-01-07 00:11:44 -0800

    second heroku

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { render } from "react-dom";
 import "./bootstrap.min.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
